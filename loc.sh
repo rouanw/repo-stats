dfa72f224dcd7f3deb9d87b6ef01b0501821f81a
rouanw <Rouan.Wilsenach@tesglobal.com>
2020-01-24 11:29:43 +0200

    parameterise directory for loc

--- loc.sh
+++ loc.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-find .  \( -name "*.js" -o -name "*.jsx" \) -not -path "./node_modules/*" -not -path "./dist/*" | xargs wc -l | sort -nr | head -n 20
+find ${1:-.}  \( -name "*.js" -o -name "*.jsx" \) -not -path "./node_modules/*" -not -path "./dist/*" | xargs wc -l | sort -nr | head -n 20
